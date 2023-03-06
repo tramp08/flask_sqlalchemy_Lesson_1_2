146db356b83778607ecd6100c0011e0c836065c8
tramp08 <oleg@botov.org>
2023-03-06 17:05:45 +0300

    Урок-2_end

---
+++ data/category.py
@@ -0,0 +1,19 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class Category(SqlAlchemyBase):
+    __tablename__ = 'category'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True,
+                           autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+
+association_table = sqlalchemy.Table(
+    'association',
+    SqlAlchemyBase.metadata,
+    sqlalchemy.Column('news', sqlalchemy.Integer,
+                      sqlalchemy.ForeignKey('news.id')),
+    sqlalchemy.Column('category', sqlalchemy.Integer,
+                      sqlalchemy.ForeignKey('category.id'))
+)
--- data/news.py
+++ data/news.py
@@ -19,5 +19,8 @@ class News(SqlAlchemyBase):
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("users.id"))
     user = orm.relationship('User')
+    categories = orm.relationship("Category",
+                                  secondary="association",
+                                  backref="news")
     def __repr__(self):
         return f"<News> {self.id} {self.title} {self.content}"
